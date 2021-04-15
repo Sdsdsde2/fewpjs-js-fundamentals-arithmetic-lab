729fd77d91144ee1e4da3ff7460e6cf1a923591a
Sdsdsde2 <imspammails31@gmail.com>
2021-04-15 15:41:27 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+
+var num1, num2, multiply
+num1 = 31
+num2 = 2
+multiply = num1 * num2
+
+var random = Math.floor(Math.random()) + 1
+
+
+var mod, num3, num4
+num3 = 24
+num4 = 5
+mod = num3 % num4
+
+var max = Math.max(1,2,3,20)
